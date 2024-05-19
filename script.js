9f5e609a108b074312712484641a2d9e359be0ca
Rishikesh T P <97593006+Rishikesh-T-P@users.noreply.github.com>
2024-05-19 13:52:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function changeText() {
+    const description = document.getElementById('description');
+    description.innerText = "Rajkeshav enjoys reading books, playing cricket, and participating in science fairs. His favorite subject is Mathematics.";
+}
